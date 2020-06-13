b0deb222f77961ac6f67fc810da3f61b40a91f65
BahamutAPs <33669440+BahamutAPs@users.noreply.github.com>
2020-06-13 00:40:24 -0600

    Added gif support. Testing reveals some gifs don't copy all colors for all frames

--- fractMal.py
+++ fractMal.py
@@ -26,13 +26,12 @@ or ".jpg" in outname):
     outname += ".png"
 im = Image.open(filename)
 mask = Image.new("RGBA", im.size, (0,0,0,123))
-newIm = Image.new("RGB", (im.width**2, im.height**2))
-row = col = 0
 frames = []
 for frame in ImageSequence.Iterator(im):
+    newIm = Image.new("RGB", (im.width**2, im.height**2))
+    row = col = 0
     frame = frame.convert("RGB")
     gray = frame.copy().convert("L")
-    #newFrame = newIm.copy()
     while row < frame.height:
         while col < frame.width:
             color = Image.new("RGB", frame.size, frame.getpixel((col, row)))
@@ -45,4 +44,5 @@ for frame in ImageSequence.Iterator(im):
 if len(frames) == 1:
     frames[0].save(outname)
 else:
-    frames[0].save(outname, save_all=True, append_images=frames[1:], duration=40, loop=0)
+    frames[0].save(outname, save_all=True, optimize=False,
+    append_images=frames[1:], duration = im.info['duration'], loop=0)

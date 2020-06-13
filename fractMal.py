dbe93e73c133d5d36a7b8e6d4471c01c77f00cbb
BahamutAPs <33669440+BahamutAPs@users.noreply.github.com>
2020-06-12 23:05:20 -0600

    ALMOST added gif support. Should have no negative impact on previous functionality

--- fractMal.py
+++ fractMal.py
@@ -11,29 +11,38 @@ How to use alpha layer and Image.composite() to add a colored overlay
 https://stackoverflow.com/a/9208256
 """
 
-from PIL import Image
+from PIL import Image, ImageSequence
 Image.MAX_IMAGE_PIXELS = None
 
 __author__ = "Andrew Peña"
 __credits__ = ["Andrew Peña", "Malcolm Johnson"]
-__version__ = "0.2.1"
+__version__ = "0.2.5"
 __status__ = "Prototype"
 
 filename = input("What is the file you wish to tile?: ")
 outname = input("What do you want to save the file as?: ")
-if not (".bmp" in outname or ".png" in outname or ".jpg" in outname):
+if not (".gif" in outname or ".bmp" in outname or ".png" in outname
+or ".jpg" in outname):
     outname += ".png"
 im = Image.open(filename)
-gray = im.copy().convert("L")
 mask = Image.new("RGBA", im.size, (0,0,0,123))
 newIm = Image.new("RGB", (im.width**2, im.height**2))
 row = col = 0
-while row < im.height:
-    while col < im.width:
-        color = Image.new("RGB", im.size, im.getpixel((col, row)))
-        comp = Image.composite(gray, color, mask).convert("RGB")
-        newIm.paste(comp, (im.width * col, im.height * row))
-        col += 1
-    row += 1
-    col = 0
-newIm.save(outname)
+frames = []
+for frame in ImageSequence.Iterator(im):
+    frame = frame.convert("RGB")
+    gray = frame.copy().convert("L")
+    #newFrame = newIm.copy()
+    while row < frame.height:
+        while col < frame.width:
+            color = Image.new("RGB", frame.size, frame.getpixel((col, row)))
+            comp = Image.composite(gray, color, mask).convert("RGB")
+            newIm.paste(comp, (im.width * col, im.height * row))
+            col += 1
+        row += 1
+        col = 0
+    frames.append(newIm)
+if len(frames) == 1:
+    frames[0].save(outname)
+else:
+    frames[0].save(outname, save_all=True, append_images=frames[1:], duration=40, loop=0)

840a4d9cf40a94a691b1971a8ee1c72743aa04aa
BahamutAPs <33669440+BahamutAPs@users.noreply.github.com>
2020-06-12 20:47:28 -0600

    Added references used, re-organized modules, cleaned up code, and renamed obscure variables

--- fractMal.py
+++ fractMal.py
@@ -1,32 +1,39 @@
-__author__ = "Andrew Peña"
-__credits__ = ["Andrew Peña", "Malcolm Johnson"]
-__version__ = "0.2.0"
-__status__ = "Prototype"
+#!/usr/bin/env python
+"""Creates a large image out of a recolored, tiled array of itself
+
+This program takes an image and tiles it, vertically and horizontally, and
+recolors each tile to match the RGB value of the corresponding pixel from the
+original image. RGB images of over 128x128 run into memory issues at the
+moment.
+
+Resources used:
+How to use alpha layer and Image.composite() to add a colored overlay
+https://stackoverflow.com/a/9208256
+"""
+
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = None
 
-"""This program takes an image and tiles it, vertically and horizontally, and
-recolors each tile to match the RGB value of the corresponding pixel from the
-original image. RGB images of over 128x128 run into memory issues at the moment.
-"""
+__author__ = "Andrew Peña"
+__credits__ = ["Andrew Peña", "Malcolm Johnson"]
+__version__ = "0.2.1"
+__status__ = "Prototype"
+
 filename = input("What is the file you wish to tile?: ")
 outname = input("What do you want to save the file as?: ")
 if not (".bmp" in outname or ".png" in outname or ".jpg" in outname):
     outname += ".png"
-    print(outname)
-
 im = Image.open(filename)
-bw = im.copy().convert("L")
+gray = im.copy().convert("L")
 mask = Image.new("RGBA", im.size, (0,0,0,123))
 newIm = Image.new("RGB", (im.width**2, im.height**2))
-r = 0
-c = 0
-while r < im.height:
-    while c < im.width:
-        color = Image.new("RGB", im.size, im.getpixel((c, r)))
-        comp = Image.composite(bw, color, mask).convert("RGB")
-        newIm.paste(comp, (im.width * c, im.height * r))
-        c += 1
-    r += 1
-    c = 0
+row = col = 0
+while row < im.height:
+    while col < im.width:
+        color = Image.new("RGB", im.size, im.getpixel((col, row)))
+        comp = Image.composite(gray, color, mask).convert("RGB")
+        newIm.paste(comp, (im.width * col, im.height * row))
+        col += 1
+    row += 1
+    col = 0
 newIm.save(outname)

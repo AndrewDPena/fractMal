69460accff5e40514ee28e3c60af1053dc227a29
BahamutAPs <33669440+BahamutAPs@users.noreply.github.com>
2020-06-18 20:04:44 -0600

    Increased gif capabilities, should now correctly convert transparent pixels in source image to transparent grayscale tiles in final image

--- fractMal.py
+++ fractMal.py
@@ -6,6 +6,14 @@ recolors each tile to match the RGB value of the corresponding pixel from the
 original image. RGB images of over 128x128 run into memory issues at the
 moment.
 
+Current Issues:
+- Works poorly on gifs with movement across a transparent background, since it
+simply pastes each frame over the previous frame
+
+- That save function looks like garbage. There should be a way to have one save
+function for gifs where transparent is only passed as a parameter if
+isTransparent is true
+
 Resources used:
 How to use alpha layer and Image.composite() to add a colored overlay
 https://stackoverflow.com/a/9208256
@@ -16,7 +24,7 @@ Image.MAX_IMAGE_PIXELS = None
 
 __author__ = "Andrew Peña"
 __credits__ = ["Andrew Peña", "Malcolm Johnson"]
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 __status__ = "Prototype"
 
 filename = input("What is the file you wish to tile?: ")
@@ -26,22 +34,28 @@ or ".jpg" in outname):
     outname += ".png"
 im = Image.open(filename)
 mask = Image.new("RGBA", im.size, (0,0,0,123))
-previousFrame = im.convert("RGBA")
+previousFrame = ImageSequence.Iterator(im)[0].convert("RGBA")
 frames = []
+isTransparent = False
+transparencyXY = (0, 0)
 for frame in ImageSequence.Iterator(im):
     newIm = Image.new("RGBA", (im.width**2, im.height**2), (0,0,0,0))
     row = col = 0
-    previousFrame.alpha_composite(frame)
-    gray = previousFrame.copy().convert("LA")
+    previousFrame.alpha_composite(frame.convert("RGBA"))
+    grayT = previousFrame.convert("LA")
+    grayF = previousFrame.convert("L")
     while row < frame.height:
         while col < frame.width:
             prevRGBA = previousFrame.getpixel((col, row))
-            tempGray = gray
+            gray = grayF
             if ((prevRGBA[3] == 0)):
-                tempGray = gray.convert("L")
-                prevRGBA = (0, 0, 0, 0)
+                if not isTransparent:
+                    isTransparent = True
+                    transparencyXY = (col, row)
+                gray = grayT
+                prevRGBA = (75, 75, 75, 0)
             color = Image.new("RGBA", frame.size, prevRGBA)
-            comp = Image.composite(tempGray, color, mask).convert("RGBA")
+            comp = Image.composite(gray, color, mask).convert("RGBA")
             newIm.paste(comp, (im.width * col, im.height * row))
             col += 1
         row += 1
@@ -50,5 +64,13 @@ for frame in ImageSequence.Iterator(im):
 if len(frames) == 1:
     frames[0].save(outname)
 else:
-    frames[0].save(outname, save_all=True, optimize=False,
-    append_images=frames[1:], duration = im.info['duration'], loop=0)
+    if isTransparent:
+        tpLoc = frames[0].convert("P").getpixel(transparencyXY)
+        frames[0].save(outname, save_all=True, optimize=True,
+        append_images=frames[1:], backgound=im.info['background'],
+        transparency=tpLoc, duration = im.info['duration'],
+        loop=0)
+    else:
+        frames[0].save(outname, save_all=True, optimize=True,
+        append_images=frames[1:], backgound=im.info['background'],
+        duration = im.info['duration'], loop=0)

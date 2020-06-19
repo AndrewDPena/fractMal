37c80deed25d074b470e13f0a07a5e648575c444
BahamutAPs <33669440+BahamutAPs@users.noreply.github.com>
2020-06-18 22:54:38 -0600

    Changed default transparency mode to fully match transparency of original image. Previous functionality added is now optional at runtime. Also added better comments, and the ability to save individual frames.

--- fractMal.py
+++ fractMal.py
@@ -10,10 +10,6 @@ Current Issues:
 - Works poorly on gifs with movement across a transparent background, since it
 simply pastes each frame over the previous frame
 
-- That save function looks like garbage. There should be a way to have one save
-function for gifs where transparent is only passed as a parameter if
-isTransparent is true
-
 Resources used:
 How to use alpha layer and Image.composite() to add a colored overlay
 https://stackoverflow.com/a/9208256
@@ -24,53 +20,66 @@ Image.MAX_IMAGE_PIXELS = None
 
 __author__ = "Andrew Peña"
 __credits__ = ["Andrew Peña", "Malcolm Johnson"]
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 __status__ = "Prototype"
 
 filename = input("What is the file you wish to tile?: ")
 outname = input("What do you want to save the file as?: ")
+fulltile = input("Enter 'y' if you want a full tile: ").lower()
 if not (".gif" in outname or ".bmp" in outname or ".png" in outname
-or ".jpg" in outname):
+or ".jpg" in outname): # Gives a default filetype of .png
     outname += ".png"
 im = Image.open(filename)
-mask = Image.new("RGBA", im.size, (0,0,0,123))
+# Changing the mask alpha changes output. Lower alpha, more color but less gif
+# clarity in the tiles.
+mask = Image.new("RGBA", im.size, (0,0,0,50))
 previousFrame = ImageSequence.Iterator(im)[0].convert("RGBA")
 frames = []
-isTransparent = False
+# Possibly unnecessary, this is used to distinguish between gifs with and
+# without transparency, which matters during the save process mostly. The XY
+# is used to locate the transparent pixel in the palette.
+isTransparentGIF = False
 transparencyXY = (0, 0)
 for frame in ImageSequence.Iterator(im):
     newIm = Image.new("RGBA", (im.width**2, im.height**2), (0,0,0,0))
     row = col = 0
+    # alpha_composite allows partial/additive gifs to work, but breaks gifs
+    # with motion over a transparent background. This needs to be re-thought.
     previousFrame.alpha_composite(frame.convert("RGBA"))
-    grayT = previousFrame.convert("LA")
+    # previousFrame = frame.convert("RGBA") # This doesn't work
+    if fulltile == 'y':
+        grayT = previousFrame.convert("LA")
+    else:
+        grayT = Image.new("RGBA", (previousFrame.size), (0,0,0,0))
     grayF = previousFrame.convert("L")
     while row < frame.height:
         while col < frame.width:
-            prevRGBA = previousFrame.getpixel((col, row))
-            gray = grayF
-            if ((prevRGBA[3] == 0)):
-                if not isTransparent:
-                    isTransparent = True
+            pixelRGBA = previousFrame.getpixel((col, row))
+            if ((pixelRGBA[3] == 0)):
+                if not isTransparentGIF:
+                    isTransparentGIF = True
                     transparencyXY = (col, row)
                 gray = grayT
-                prevRGBA = (75, 75, 75, 0)
-            color = Image.new("RGBA", frame.size, prevRGBA)
+                pixelRGBA = (0,0,0,0)
+            else:
+                gray = grayF
+            color = Image.new("RGBA", frame.size, pixelRGBA)
             comp = Image.composite(gray, color, mask).convert("RGBA")
             newIm.paste(comp, (im.width * col, im.height * row))
             col += 1
         row += 1
         col = 0
+    # Un-comment the next line to have access to each individual frame
+    # newIm.save("Frame" + str(frame.tell()+1) + ".png")
     frames.append(newIm)
 if len(frames) == 1:
     frames[0].save(outname)
 else:
-    if isTransparent:
+    frames[0].save(outname, save_all = True, optimize=True,
+    append_images=frames[1:], backgound=im.info['background'],
+    duration = im.info['duration'], loop=0)
+    if isTransparentGIF:
         tpLoc = frames[0].convert("P").getpixel(transparencyXY)
-        frames[0].save(outname, save_all=True, optimize=True,
-        append_images=frames[1:], backgound=im.info['background'],
-        transparency=tpLoc, duration = im.info['duration'],
-        loop=0)
-    else:
-        frames[0].save(outname, save_all=True, optimize=True,
-        append_images=frames[1:], backgound=im.info['background'],
-        duration = im.info['duration'], loop=0)
+        temp = Image.open(outname)
+        temp.info['transparency'] = tpLoc
+        temp.save("new" + outname, save_all=True)
